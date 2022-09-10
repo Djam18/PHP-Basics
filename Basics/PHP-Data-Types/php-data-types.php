ee93675b07fb9f62eef254b65db7ce2d0712b4df
ADAM Abdel <adamdjamal18@gmail.com>
2022-09-10 21:33:40 +0100

    PHP-Data-Types

---
+++ Basics/PHP-Data-Types/php-data-types.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> PHP Data Types </title>
+</head>
+<body>
+    <?php
+    $helloVariable = "Hello you!";
+    $hello_variable = 'Hello you!';
+    $x = 8;
+    $is_human = true;
+    $car_list = ['Lamborgini','Ford','Porsche'];
+    $null_variable = NULL;
+    $y = 10.0;
+    echo $x;
+    echo "<br>";
+    echo $y;
+    echo "<br>";
+    print($hello_variable);
+    echo "<br>";
+    print($helloVariable);
+    echo "<br>";
+    print($is_human);
+    echo "<br>";
+    print($null_variable);
+    echo "<br>";
+    print($car_list);    
+    ?>
+</body>
+</html>
