92a3c62484447b3de080ed0b19be7579c548f1ab
ADAM Abdel <adamdjamal18@gmail.com>
2022-09-10 20:42:59 +0100

    Initial commit

---
+++ Basics/PHP comments/comments.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Comments</title>
+</head>
+<body>
+    A comment in PHP code is a line that is not executed as a part of the program. Its only purpose is to be read by someone who is looking at the code.
+    Comments can be used to:
+        -Let others understand your code
+        -Remind yourself of what you did - Most programmers have experienced coming back to their own work a year or two later and having to re-figure out what they did. Comments can remind you of what you were thinking when you wrote the code
+        
+    PHP supports several ways of commenting:    
+    <?php
+
+        //this a comment. More exactly a line comment.
+        /*
+          This a comment too. (Multiline comment)
+         */
+    ?>
+
+</body>
+</html>
---
+++ Basics/PHP variables/variable.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Variables</title>
+</head>
+<body>
+
+    <div>
+        Variables are "containers" for storing information.
+    </div>
+    <h1>Creating (Declaring) PHP Variables</h1>
+    <p>
+        In PHP, a variable starts with the <b>$</b> sign, followed by the name of the variable:
+    </p>
+    <?php
+        $txt = "Hello world!"; //  variable containing String  value
+        $x = 5; // variable containing integer value
+        $y = 10.5; // variable containing float value
+    ?>
+
+</body>
+</html>
